df9a01a245bcfa108a5c1b1f5a1a2cce05c65a8b
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2022-03-01 20:30:44 -0600

    update go brr

--- contracts/Image.sol
+++ contracts/Image.sol
@@ -2,44 +2,45 @@
 pragma solidity ^0.8.10;
 
 contract Image {
-    struct PixelChunk { // 216-bit chunk - 3x3 chunk
-        uint8[3][3] red;
-        uint8[3][3] green;
-        uint8[3][3] blue;
+    struct Pixel {
+        uint8 red;
+        uint8 green;
+        uint8 blue;
     }
-
-    mapping(uint256 => mapping(uint256 => PixelChunk)) internal imageChunk; // imageChunk[x][y] = PixelChunk
-
-    // Super expensive method 90K gas/pixel
-    function editChunk(uint256 _chunkX, uint256 _chunkY, uint8 _x, uint8 _y, uint8 _red, uint8 _green, uint8 _blue) external {
-        PixelChunk storage pChunk = imageChunk[_chunkX][_chunkY];
-
-        (pChunk.red[_x][_y], pChunk.green[_x][_y], pChunk.blue[_x][_y]) = (_red, _green, _blue);
+    
+    mapping(uint256 => mapping(uint256 => Pixel)) internal image;
+    
+    // zero-value -> non-zero = 44.6K
+    // non-zero -> non-zero = 27.5K
+    // non-zero -> zero-value = 22.7K
+    function editPixel(uint256 _x, uint256 _y, uint8 _red, uint8 _green, uint8 _blue) external {
+        Pixel storage pixel = image[_x][_y];
+        
+        pixel.red = _red;
+        pixel.green = _green;
+        pixel.blue = _blue;
     }
+    
+    // zero-value -> non-zero = 24.97K/pixel (16 pixels changed)
+    // non-zero -> non-zero = 9.88/pixel (16 pixels changed)
+    // non-zero -> zero-value = 7.13K/pixel (16 pixels changed)
+    function editPixel(uint256[] memory _x, uint256[] memory _y, uint8[][] memory _red, uint8[][] memory _green, uint8[][] memory _blue) external {
 
-    // Somewhat cheaper 34-36K gas/pixel
-    function editChunk(uint256 _chunkX, uint256 _chunkY, uint8[3][3] memory _red, uint8[3][3] memory _green, uint8[3][3] memory _blue) external {
-        PixelChunk storage pChunk = imageChunk[_chunkX][_chunkY];
-
-        pChunk.red = _red;
-        pChunk.green = _green;
-        pChunk.blue = _blue;
-    }
-
-    function viewChunk(uint256 _chunkX, uint256 _chunkY) external view returns(PixelChunk memory) {
-        return imageChunk[_chunkX][_chunkY];
-    }
-
-    // good luck
-    function viewEntireImage(uint256 _maxChunkX, uint256 _maxChunkY) external view returns(PixelChunk[][] memory) {
-        PixelChunk[][] memory image = new PixelChunk[][](_maxChunkX * _maxChunkY);
-
-        for(uint256 x; x < _maxChunkX; x++) {
-            for(uint256 y; y < _maxChunkY; y++) {
-                image[x][y] = imageChunk[x][y];
+        unchecked {
+            uint256 x;
+            uint256 y;
+            for(uint256 i; i < _x.length; i++) {
+                x = _x[i];
+                y = _y[i];
+                
+                Pixel storage pixel = image[x][y];
+                
+                pixel.red = _red[0][i];
+                pixel.green = _green[0][i];
+                pixel.blue = _blue[0][i];
+                
             }
         }
-
-        return image;
+        
     }
 }
